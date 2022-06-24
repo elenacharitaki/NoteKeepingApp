c75c12b389c14d73a38357ae92a8135df18fe755
Elena <elenaxar00@gmail.com>
2022-06-24 13:56:39 +0300

    Add SQL delete

---
+++ NoteKeepingApp/DBHelper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace NoteKeepingApp
+{
+    internal class DBHelper
+    {
+        static string connectionString = Properties.Settings.Default.NotesDbConnectionString;
+
+        public static void InsertToDB(string title, string body)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sql = "INSERT INTO Notes (Title, Body) VALUES(@Title, @Body)";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@Title", title);
+                    command.Parameters.AddWithValue("@Body", body);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static void UpdateNoteInDb(int id, string title, string body)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sql = String.Format("UPDATE Notes SET Title='{0}', Body='{1}' WHERE NoteId='{2}'", title, body, id);
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static List<Note> SelectAllNotesFromDb()
+        {
+            List<Note> result = new List<Note>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sql = "SELECT * FROM Notes";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Note note = new Note();
+                        note.Id = reader.GetInt32(0);
+                        note.Title = reader.GetString(1);
+                        note.Body = reader.GetString(2);
+
+                        result.Add(note);
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static void DeleteFromDb(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sql = String.Format("DELETE FROM Notes WHERE NoteId='{0}'", id);
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
--- NoteKeepingApp/Home.cs
+++ NoteKeepingApp/Home.cs
@@ -30,35 +30,16 @@ namespace NoteKeepingApp
 
             if(update == true)
             {
-                UpdateNoteInDb(noteId, noteTitle, noteBody);
+                DBHelper.UpdateNoteInDb(noteId, noteTitle, noteBody);
                 update = false;
             }
             else
             {
-                InsertToDB(noteTitle, noteBody);
-            }
-            
-            tbNoteTitle.Text = "";
-            rtbNoteBody.Text = "";
-        }
-
-        private void InsertToDB(string noteTitle, string noteBody)
-        {
-            string connectionString = Properties.Settings.Default.NotesDbConnectionString;
-            using(SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-                string sql = "INSERT INTO Notes (Title, Body) VALUES(@Title, @Body)";
-                using( SqlCommand command = new SqlCommand(sql, connection))
-                {
-                    command.Parameters.AddWithValue("@Title", noteTitle);
-                    command.Parameters.AddWithValue("@Body", noteBody);
-                    command.ExecuteNonQuery();
-                }
+                DBHelper.InsertToDB(noteTitle, noteBody);
             }
             LoadNoteList();
+            ClearForm();
         }
-
         private void Home_Load(object sender, EventArgs e)
         {
             LoadNoteList();
@@ -70,26 +51,8 @@ namespace NoteKeepingApp
             lbAllNotes.DisplayMember = "Title";
             lbAllNotes.ValueMember = "Id";
 
-            string connectionString = Properties.Settings.Default.NotesDbConnectionString;
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-                string sql = "SELECT * FROM Notes";
-                using (SqlCommand command = new SqlCommand(sql, connection))
-                {
-                    SqlDataReader reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        Note note = new Note();
-                        note.Id = reader.GetInt32(0);
-                        note.Title = reader.GetString(1);
-                        note.Body = reader.GetString(2);
-
-                        notes.Add(note);
-                    }
-                    lbAllNotes.DataSource = notes;
-                }
-            }
+            notes = DBHelper.SelectAllNotesFromDb();
+            lbAllNotes.DataSource = notes;
         }
 
         private void btnEdit_Click(object sender, EventArgs e)
@@ -101,19 +64,18 @@ namespace NoteKeepingApp
             update = true;
         }
 
-        private void UpdateNoteInDb(int id, string title, string body)
+        private void ClearForm()
         {
-            string connectionString = Properties.Settings.Default.NotesDbConnectionString;
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-                string sql = String.Format("UPDATE Notes SET Title='{0}', Body='{1}' WHERE NoteId='{2}'", title, body, id);
-                using (SqlCommand command = new SqlCommand(sql, connection))
-                {
-                    command.ExecuteNonQuery();
-                }
-                LoadNoteList();
-            }
+            tbNoteTitle.Text = "";
+            rtbNoteBody.Text = "";
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            Note selectedNote = (Note)lbAllNotes.SelectedItem;
+            noteId = selectedNote.Id;
+            DBHelper.DeleteFromDb(noteId);
+            LoadNoteList();
         }
     }
 }
